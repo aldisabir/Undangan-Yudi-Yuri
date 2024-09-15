dc62f41f78ccdb450c3dc1c66f4d45ba61258a91
aldisabir <risaldi.irsya@gmail.com>
2024-09-16 03:06:20 +0700

    undangan-pertama

---
+++ main.js
@@ -0,0 +1,19 @@
+let audio = document.getElementById ('bg-audio')
+const playButton = document.getElementById ('play-btn');
+const pauseButton = document.getElementById ('pause-btn');
+
+
+playButton.addEventListener('click', function() {
+
+    playButton.className = 'hidden'
+    pauseButton.className = 'content min-h-9 min-w-6 max-w-9 rounded-full border-4 border-double bg-black text-sm text-yellow-500 border-yellow-500 py-1 px-2'
+    audio.muted = true;
+})
+
+pauseButton.addEventListener('click', function() {
+
+    playButton.className = 'content min-h-6 min-w-6 max-w-9 rounded-full border-4 border-double bg-black text-sm text-yellow-500 border-yellow-500 py-1 px-2'
+    pauseButton.className = 'hidden'
+    audio.muted = false;
+})
+
